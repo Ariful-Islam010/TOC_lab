1865792c8d46282f40957993224f4e57e46894cd
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-07 23:29:41 +0600

    Create 26.Strings that accept "11" or "111".cpp

---
+++ 26.Strings that accept "11" or "111".cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(s=="11"||s=="111" )
+      cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    return 0;
+}

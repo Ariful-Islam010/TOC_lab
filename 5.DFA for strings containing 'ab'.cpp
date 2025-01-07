6fa56d58df45b7bf068477dadc23a3ca09800e1a
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-07 22:08:56 +0600

    Create 5.DFA for strings containing 'ab'.cpp

---
+++ 5.DFA for strings containing 'ab'.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length()-1;i++)
+    {
+       if(s[i]=='a' && s[i+1]=='b')
+       {
+        cout<<"Accepted"<<endl;
+        return 0;
+       }
+    }
+    cout<<"Rejected"<<endl;
+    return 0;
+}

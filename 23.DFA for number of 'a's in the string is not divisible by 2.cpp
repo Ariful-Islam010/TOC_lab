e90916bb9d84ccf2abf7bd1a66643c1773021387
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-07 23:20:49 +0600

    Create 23.DFA for number of 'a's in the string is not divisible by 2.cpp

---
+++ 23.DFA for number of 'a's in the string is not divisible by 2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int count=0;
+    int len=s.length();
+    for(int i=0;i<len;i++)
+    {
+        if(s[i]=='a')
+            count++;
+    }
+    if(count%2!=0)
+      cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    return 0;
+}

8a8565c627e7d7b79234eb92fc6ea491ef4b3e02
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-07 23:03:02 +0600

    Create 14.DFA for strings of length at least 2.cpp

---
+++ 14.DFA for strings of length at least 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int len=s.length();
+    if(len>=2)
+      cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    return 0;
+}

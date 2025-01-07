ac62fdae6502cccb020bcd34d7589f56d87475d7
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-07 22:02:11 +0600

    Create 3.DFA for strings starting with 'aab'.cpp

---
+++ 3.DFA for strings starting with 'aab'.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(s[0]=='a' && s[1]=='a' && s[2]=='b')
+        cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    return 0;
+}

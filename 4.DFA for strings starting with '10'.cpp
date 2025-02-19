063d67cb5191d22e2cb411894761d77f622cd0ce
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-02-19 21:21:09 +0600

    Update 4.DFA for strings starting with '10'.cpp

--- 4.DFA for strings starting with '10'.cpp
+++ 4.DFA for strings starting with '10'.cpp
@@ -3,10 +3,29 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    if(s[0]=='1' && s[1]=='0')
+    int i,count;
+    while(1)
+    {
+        cout<<"Enter the string : ";
+        cin>>s;
+        count=0;
+        int len=s.length();
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]=='1'||s[i]=='0')
+             continue;
+            else
+            {
+             count=1;
+             break;
+            }
+        }
+     if(count==1)
+        cout<<"You put wrong Alphabet."<<endl;
+     else if(s[0]=='1' && s[1]=='0')
         cout<<"Accepted"<<endl;
-    else
+     else
         cout<<"Rejected"<<endl;
-    return 0;
+    }
 }
+

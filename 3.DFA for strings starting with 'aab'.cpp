5043299fa8a144633e3b2d07f5398daf352821d1
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-02-19 21:15:30 +0600

    Update 3.DFA for strings starting with 'aab'.cpp

--- 3.DFA for strings starting with 'aab'.cpp
+++ 3.DFA for strings starting with 'aab'.cpp
@@ -3,10 +3,28 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    if(s[0]=='a' && s[1]=='a' && s[2]=='b')
+    int i,count=0;
+    while(1)
+    {
+        cout<<"Enter the string : ";
+        cin>>s;
+        int len=s.length();
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]=='a'||s[i]=='b')
+             continue;
+            else
+            {
+             count=1;
+             break;
+            }
+        }
+     if(count==1)
+        cout<<"You put wrong Alphabet."<<endl;
+     else if(s[0]=='a' && s[1]=='a' && s[2]=='b')
         cout<<"Accepted"<<endl;
-    else
+     else
         cout<<"Rejected"<<endl;
-    return 0;
+    }
 }
+

9bbdb24f047607b6cc0c37eb35989d40b8460112
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 02:53:51 +0600

    Update 9.DFA for strings starting and ending with different symbols.cpp

--- 9.DFA for strings starting and ending with different symbols.cpp
+++ 9.DFA for strings starting and ending with different symbols.cpp
@@ -3,11 +3,29 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
+    while(1)
+    {
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0;
     int len=s.length();
-    if(s[0]!=s[len-1])
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='a'||s[i]=='b')
+            continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(s[0]!=s[len-1] && len>=2 )
         cout<<"Accepted"<<endl;
     else
         cout<<"Rejected"<<endl;
+    }
     return 0;
 }

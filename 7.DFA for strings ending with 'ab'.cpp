f5b0ac859226c94ddbab8a5b66f4a457e2027c39
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 02:19:27 +0600

    Update 7.DFA for strings ending with 'ab'.cpp

--- 7.DFA for strings ending with 'ab'.cpp
+++ 7.DFA for strings ending with 'ab'.cpp
@@ -3,11 +3,28 @@ using namespace std;
 int main()
 {
     string s;
+    while(1)
+    {
+    cout<<"Enter the string : ";
     cin>>s;
+    int i,count=0;
     int len=s.length();
-    if(s[len-2]=='a' && s[len-1]=='b' )
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='a'||s[i]=='b')
+            continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(s[len-2]=='a' && s[len-1]=='b' && len>=2)
         cout<<"Accepted"<<endl;
     else
         cout<<"Rejected"<<endl;
+    }
     return 0;
 }

b0845d63f7812157a39ea9cee76216397ba354e3
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 02:17:28 +0600

    Update 6.DFA for strings containing 'baba'.cpp

--- 6.DFA for strings containing 'baba'.cpp
+++ 6.DFA for strings containing 'baba'.cpp
@@ -3,15 +3,37 @@ using namespace std;
 int main()
 {
     string s;
+    while(1)
+    {
+    cout<<"Enter the string : ";
     cin>>s;
-    for(int i=0;i<s.length()-3;i++)
+    int i,count=0;
+    int len=s.length();
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='a'||s[i]=='b')
+            continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    int flag=0;
+    for(i=0;i<len-3;i++)
     {
        if(s[i]=='b' && s[i+1]=='a' && s[i+2]=='b' && s[i+3]=='a')
        {
-        cout<<"Accepted"<<endl;
-        return 0;
+        flag=1;
+        break;
        }
     }
-    cout<<"Rejected"<<endl;
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(flag==1 && len>=2)
+        cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    }
     return 0;
 }

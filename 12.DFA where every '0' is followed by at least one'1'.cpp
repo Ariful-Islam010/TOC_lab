5472672802bb67c72db8bf66f849c716362b783c
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 10:24:03 +0600

    Update 12.DFA where every '0' is followed by at least one'1'.cpp

--- 12.DFA where every '0' is followed by at least one'1'.cpp
+++ 12.DFA where every '0' is followed by at least one'1'.cpp
@@ -3,15 +3,45 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    for(int i=0;i<s.length();i++)
+    while(1)
     {
-        if(s[i]=='0' && s[i+1]!='1')
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0;
+    int len=s.length();
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='0'||s[i]=='1')
+            continue;
+        else
         {
-            cout<<"Rejected"<<endl;
-            return 0;
+           count=1;
+           break;
         }
     }
-    cout<<"Accepted"<<endl;
+    int flag=0;
+    for(i=0;i<len-1;i++)
+    {
+        if(s[i]=='0' && s[i+1]=='1')
+        {
+          flag=1;
+          i++;
+        }
+        else if(s[i]=='1')
+            continue;
+        else
+        {
+            flag=0;
+            break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if((flag==1 || s=="") && s[len-1]!='0')
+        cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    }
     return 0;
 }

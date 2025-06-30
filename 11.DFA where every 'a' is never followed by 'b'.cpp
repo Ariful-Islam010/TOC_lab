61424cd882417e62fa24a24eab7c2b0e258f9ac0
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 10:21:16 +0600

    Update 11.DFA where every 'a' is never followed by 'b'.cpp

--- 11.DFA where every 'a' is never followed by 'b'.cpp
+++ 11.DFA where every 'a' is never followed by 'b'.cpp
@@ -3,15 +3,40 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    for(int i=0;i<s.length();i++)
+    while(1)
+    {
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0;
+    int len=s.length();
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='a'||s[i]=='b')
+            continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    int flag=0;
+    for(i=0;i<len-1;i++)
     {
         if(s[i]=='a' && s[i+1]=='b')
         {
-            cout<<"Rejected"<<endl;
-            return 0;
+          flag=1;
+          break;
         }
+        else
+            continue;
     }
-    cout<<"Accepted"<<endl;
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(flag==0||s=="")
+        cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    }
     return 0;
 }

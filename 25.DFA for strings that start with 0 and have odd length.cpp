fe6562bfa0c13de1955afa57f9aaa36119789c8d
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 10:47:13 +0600

    Update 25.DFA for strings that start with 0 and have odd length.cpp

--- 25.DFA for strings that start with 0 and have odd length.cpp
+++ 25.DFA for strings that start with 0 and have odd length.cpp
@@ -3,12 +3,29 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    int count=0;
+    while(1)
+    {
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0,counta=0;
     int len=s.length();
-    if(s[0]=='0' && len%2==1)
-      cout<<"Accepted"<<endl;
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='0' || s[i]=='1')
+          continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(s[0]=='0' && len%2==1)
+        cout<<"Accepted"<<endl;
     else
         cout<<"Rejected"<<endl;
+    }
     return 0;
 }

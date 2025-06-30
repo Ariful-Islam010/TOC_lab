2791211df0383db993a64b9cb4ded8d085eec93d
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-06-30 10:50:30 +0600

    Update 27.DFA for strings of the binary number that are divisible by 2.cpp

--- 27.DFA for strings of the binary number that are divisible by 2.cpp
+++ 27.DFA for strings of the binary number that are divisible by 2.cpp
@@ -3,12 +3,29 @@ using namespace std;
 int main()
 {
     string s;
-    cin>>s;
+    while(1)
+    {
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0;
     int len=s.length();
-    //Ends with '0'
-    if(s[len-1]=='0')
-      cout<<"Accepted"<<endl;
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='0' || s[i]=='1')
+          continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(s[len-1]=='0')
+        cout<<"Accepted"<<endl;
     else
         cout<<"Rejected"<<endl;
+    }
     return 0;
 }
